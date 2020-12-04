fcd4f121e50d1c211bc63418090dfe5112ff2061
masterraf21 <masterraf21@gmail.com>
2020-12-04 22:54:40 +0700

    feat(error): add error handler

---
+++ backend/src/errors/error.ts
@@ -0,0 +1,34 @@
+import { Response } from 'express'
+
+export function badRequest(err: any, res: Response) {
+  res.status(400).json({
+    message: 'Bad Request',
+    error: err
+  })
+}
+
+export function notFoundError(notFound: string, res: Response) {
+  res.status(404).json({
+    message: `${notFound} not found`
+  })
+}
+
+export function invalidCredentials(res: Response) {
+  res.status(401).json({
+    message: 'Invalid Credentials'
+  })
+}
+
+export function unAuthorizedError(res: Response) {
+  res.status(403).json({
+    message: 'UnAuthorized Error',
+    error: 'Access Denied'
+  })
+}
+
+export function internalServerError(err: any, res: Response) {
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err
+  })
+}
